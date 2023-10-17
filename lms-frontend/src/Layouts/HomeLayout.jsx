b416e103fcf6a076c1538ac68ccee9ff7f23bd09
vinayVishwakarmaa <vinaykumar843569@gmail.com>
2023-10-18 03:21:34 +0530

    Added login singup buttons to sidebar

--- lms-frontend/src/Layouts/HomeLayout.jsx
+++ lms-frontend/src/Layouts/HomeLayout.jsx
@@ -1,22 +1,46 @@
 import React from "react";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
-import Footer from '../Components/Footer'
+import { Link, useNavigate } from "react-router-dom";
+import Footer from "../Components/Footer";
+import { useDispatch, useSelector } from 'react-redux';
 
-function HomeLayout({children}) {
+
+function HomeLayout({ children }) {
+  const dispatch = useDispatch();
+  const naviagte = useNavigate();
+
+  // for checking if user is logged
+  const isLoggedIn = useSelector((state)=> state?.auth?.isLoggedIn);
+
+
+
+// For Displaying the options accecting to role
+
+  const role = useSelector((state) => state?.auth?.role);
+
+
   function changeWidth() {
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = 'auto';
+    drawerSide[0].style.width = "auto";
   }
 
   function hideDrawer() {
     const element = document.getElementsByClassName("drawer-toggle");
     element[0].checked = false;
     const drawerSide = document.getElementsByClassName("drawer-side");
-    drawerSide[0].style.width = '0';
+    drawerSide[0].style.width = "0";
   }
+
    
+  function handleLogout(e) {
+    e.preventDefault();
+
+    // const res = await dispatch(logout())
+
+    // if(res?.payload?.success)
+    naviagte("/");
+  }
 
 
   return (
@@ -38,27 +62,61 @@ function HomeLayout({children}) {
             <ul className="menu p-4 w-48 sm:w-80 bg-base-200 text-base-content relative">
               <li className=" w-fit absolute right-2 z-50">
                 <button onClick={hideDrawer}>
-                  <AiFillCloseCircle size={24}/>
+                  <AiFillCloseCircle size={24} />
                 </button>
               </li>
               <li>
-              <Link to="/">Home</Link>
+                <Link to="/">Home</Link>
+              </li>
+
+              {isLoggedIn && role === 'ADMIN' && (
+                <li>
+                  <Link to="/admin/dashboard">Admin DashBoard</Link>
+                </li>
+               )}
+
+              <li>
+                <Link to="/courses">All Courses</Link>
               </li>
               <li>
-               <Link to="/courses">All Courses</Link>
+                <Link to="/contact">Contact Us</Link>
               </li>
               <li>
-               <Link to="/contact">Contact Us</Link>
+                <Link to="/about">About Us</Link>
               </li>
-              <li>
-               <Link to="/about">About Us</Link>
-              </li>
+
+              {!isLoggedIn && (
+                <li className=" absolute bottom-4 w-[90%">
+                <div className=" w-full flex items-center justify-center">
+                  <button className=" btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/login">Login</Link>
+                  </button>
+                  <button className=" btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/login">SignUp</Link>
+                  </button>
+                  </div>
+                  </li>
+               )}
+
+               {isLoggedIn && (
+                <li className=" absolute bottom-4 w-[90%">
+                <div className=" w-full flex items-center justify-center">
+                  <button className=" btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link to="/user/profile">Profile</Link>
+                  </button>
+                  <button className=" btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                    <Link onClick={handleLogout} >Logout</Link>
+                  </button>
+                  </div>
+                  </li>
+               )}
+              
             </ul>
           </div>
         </div>
 
         {children}
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
